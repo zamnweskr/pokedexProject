57b8015d3c3bdaa3ae4d04e70f3fb464eb2a3650
ShmeakShmcok <shmocktography@gmail.com>
2026-02-25 09:35:55 -0800

    Add Call

--- API/calls.js
+++ API/calls.js
@@ -19,6 +19,29 @@ export const getPokemonByName = async (name) => {
     }
     
 }
+
+export const getPokemonMoves = async (move) => {
+    try{
+       const response = await axios.get(`https://pokeapi.co/api/v2/move?limit=150`)
+        return response.data.results
+    } catch(e) {
+        console.log(e)
+        alert(`Sorry no moves found.`)
+    }
+    }
+
+    export const getPokemonMoveDetails = async (url) => {
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (e) {
+    console.error(e);
+    alert('Sorry, could not fetch move details.');
+    
+  }
+};
+
+
 // I dont think I will need this function anymore as we arent going by pages and we can just render all the pokemon at once in a flatlist 
 
 // export const getPokemonByPage = async(Page = 10) => {
